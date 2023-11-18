8410594456a54c4e65bfcbf0cefe62316255e17e
Hayward_chan <107766018+Haywardchan@users.noreply.github.com>
2023-11-19 00:57:29 +0800

    Create Info_Widget_Mobile_Model.dart

---
+++ Team148_FrontEnd/Custom_Widgets/Info_Widget_Mobile_Model.dart
@@ -0,0 +1,29 @@
+import '/flutter_flow/flutter_flow_charts.dart';
+import '/flutter_flow/flutter_flow_drop_down.dart';
+import '/flutter_flow/flutter_flow_icon_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/form_field_controller.dart';
+import 'info_widget_mobile_widget.dart' show InfoWidgetMobileWidget;
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class InfoWidgetMobileModel extends FlutterFlowModel<InfoWidgetMobileWidget> {
+  ///  State fields for stateful widgets in this component.
+
+  // State field(s) for DropDown widget.
+  String? dropDownValue;
+  FormFieldController<String>? dropDownValueController;
+
+  /// Initialization and disposal methods.
+
+  void initState(BuildContext context) {}
+
+  void dispose() {}
+
+  /// Action blocks are added here.
+
+  /// Additional helper methods are added here.
+}
