2977bad030f22151fc12011c91039b30f259ff4c
Hayward_chan <107766018+Haywardchan@users.noreply.github.com>
2023-11-19 00:56:13 +0800

    Create Info_Widget_Model.dart

---
+++ Team148_FrontEnd/Custom_Widgets/Info_Widget_Model.dart
@@ -0,0 +1,35 @@
+import '/flutter_flow/flutter_flow_charts.dart';
+import '/flutter_flow/flutter_flow_drop_down.dart';
+import '/flutter_flow/flutter_flow_icon_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/form_field_controller.dart';
+import 'info_widget_widget.dart' show InfoWidgetWidget;
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class InfoWidgetModel extends FlutterFlowModel<InfoWidgetWidget> {
+  ///  Local state fields for this component.
+
+  int? height = 360;
+
+  int? width = 206;
+
+  ///  State fields for stateful widgets in this component.
+
+  // State field(s) for DropDown widget.
+  String? dropDownValue;
+  FormFieldController<String>? dropDownValueController;
+
+  /// Initialization and disposal methods.
+
+  void initState(BuildContext context) {}
+
+  void dispose() {}
+
+  /// Action blocks are added here.
+
+  /// Additional helper methods are added here.
+}
